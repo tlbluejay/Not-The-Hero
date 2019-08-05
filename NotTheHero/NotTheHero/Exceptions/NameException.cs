f3e83208a56c8eb87bd93ab7b260c9f9425a52ee
rshaw02 <rshaw@student.neumont.edu>
2019-08-05 08:43:04 -0600

    Fleshed out the Entity class a little and added NameException

---
+++ NotTheHero/NotTheHero/Exceptions/NameException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NotTheHero.Exceptions
+{
+    class NameException : Exception
+    {
+        public NameException() { }
+
+        public NameException(string message) : base(message)
+        {
+
+        }
+
+        public static void CheckName(string value)
+        {
+            if (value.Length > 40) throw new NameException("name cannot be more than 40 characters");
+        }
+    }
+}
--- NotTheHero/NotTheHero/Models/Entity.cs
+++ NotTheHero/NotTheHero/Models/Entity.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using NotTheHero.Exceptions;
 
 namespace NotTheHero.Models
 {
     class Entity
     {
-        public int Health { get; set; }
-        public string Name { get; set; }
+        private int health;
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health = value;
+                if (health <= 0) dead = true;
+            }
+        }
+
+        private bool dead = false;
+        public bool Dead
+        {
+            get { return dead; }
+        }
+
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                NameException.CheckName(value);
+                name = value;
+            }
+        }
+
         public List<Item> Inventory { get; set; }
     }
 }
