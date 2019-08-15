6424de18acf5311f68e57f8bf71da20a75042f4a
rshaw02 <rshaw@student.neumont.edu>
2019-08-15 14:24:38 -0600

    Turned Entity and Minion into scripts.

---
+++ You Are !theHero/Assets/Scripts/EntityController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityController : MonoBehaviour
+{
+    public const int MIN_RANK = 0, MAX_RANK = 5;
+
+    public int maxHealth;
+
+    public new string name;
+
+    public int speed;
+    public int accuracy;
+    public int defense;
+
+    public int experience;
+    public int rank;
+
+    private int currentHealth;
+    public int Health
+    {
+        get
+        {
+            return currentHealth;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual float DefenseModifier()
+    {
+        return 1.0f;
+    }
+
+    public virtual float ActionModifier()
+    {
+        return 1.0f;
+    }
+
+    public void ChangeHealth(int diff)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + diff, 0, maxHealth);
+    }
+}
