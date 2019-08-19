c8c5973196d829acc29c6133abdbbe24fd895395
tent <tlewis@student.neumont.edu>
2019-08-19 08:14:17 -0600

    added the cryptController

---
+++ You Are !theHero/Assets/Scripts/CryptController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CryptController : MonoBehaviour
+{
+
+    //private readonly List<T> upgrades;
+    //public List<T> Upgrades
+    //{
+    //    get
+    //    {
+    //        return upgrades;
+    //    }
+    //}
+
+    //private readonly List<T> toHire;
+    //public List<T> ToHire
+    //{
+    //    get
+    //    {
+    //        return toHire;
+    //    }
+    //}
+
+    //public bool Buyable(T buying)
+    //{
+    //    return buying.cost < User.gold;
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
