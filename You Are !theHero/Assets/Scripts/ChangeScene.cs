0a54b4777e279dacb89c1423e50676b67aba6763
rshaw02 <rshaw@student.neumont.edu>
2019-08-29 00:25:27 -0600

    Buttons are retard

---
+++ You Are !theHero/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneToLoad;
+    public string sceneToUnload;
+
+    public void ChangeToScene()
+    {
+        StartCoroutine(LoadYourAsyncScene(sceneToLoad));
+        StartCoroutine(UnloadYourAsyncScene(sceneToUnload));
+    }
+
+    private IEnumerator LoadYourAsyncScene(string sceneToLoad)
+    {
+        // The Application loads the Scene in the background as the current Scene runs.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    private IEnumerator UnloadYourAsyncScene(string sceneToUnload)
+    {
+        AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync(sceneToUnload);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ You Are !theHero/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject[] Party;
+
+    public static Player instance;
+
+    public Vector2[] positions;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+        if(Party == null)
+        {
+            Party = new GameObject[4];
+            for(int i = 0; i < Party.Length; i++)
+            {
+                GameObject minion = Instantiate(Party[i], positions[i], Quaternion.identity);
+                minion.name = $"Minion {i}";
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
